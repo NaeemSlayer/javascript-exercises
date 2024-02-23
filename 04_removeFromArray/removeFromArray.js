7405bfc733ebf57e3eeaf4e8ffae90e19121b212
NaeemSlayer <140403182+NaeemSlayer@users.noreply.github.com>
2024-02-24 03:18:46 +0500

    solved removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...elementToRemove) {
+    let newArray = [];
+    for (let char of array) {
+        if (!elementToRemove.includes(char)) {
+            newArray.push(char)
+        }
+    }
+    return newArray;
+}
+console.log(removeFromArray([1,2,3,4],3,2,1,4));;
 
 // Do not edit below this line
 module.exports = removeFromArray;
