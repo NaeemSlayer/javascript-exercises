74be221042b14b6d787d062dcba8447322c4377b
NaeemSlayer <140403182+NaeemSlayer@users.noreply.github.com>
2024-02-24 03:20:03 +0500

    solved sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function (min, max) {
+    if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
+        return 'ERROR';
+    }
+    if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+}
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum = sum + i;
+    }
+        return sum;
+}
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
